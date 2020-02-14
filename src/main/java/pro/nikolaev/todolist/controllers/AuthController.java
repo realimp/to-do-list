055b774b65b023487f4bec44a4c680df578935fd
realimp <impinbox@hotmail.com>
2020-02-14 19:27:55 +0300

    Initial checkin
    Implemented basic functionality for app to load.

---
+++ src/main/java/pro/nikolaev/todolist/controllers/AuthController.java
@@ -0,0 +1,20 @@
+package pro.nikolaev.todolist.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        return "register";
+    }
+}
---
+++ src/main/java/pro/nikolaev/todolist/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package pro.nikolaev.todolist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.nikolaev.todolist.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+}
